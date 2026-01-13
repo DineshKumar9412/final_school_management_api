b0f26032a748856fff5398c1f150deccb5164382
DineshKumar9412 <83091156+DineshKumar9412@users.noreply.github.com>
2026-01-13 13:54:16 +0530

    Create session.py

---
+++ school_app/app/database/session.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy import create_engine
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
