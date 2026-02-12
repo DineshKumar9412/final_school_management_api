200388f01cb79991d7f825abaa8a6bd2512c0de4
“Dinesh” <3dinesh39412@gmail.com>
2026-02-12 12:51:05 +0530

    name changes

--- school_app/api/admin_web.py
+++ school_app/api/admin_web.py
@@ -14,9 +14,9 @@ from models.admin_models import School, SchoolGroup, SchoolStream,SchoolStreamCl
 
 
 ## ADMIN PAGE ROUTER
-admin_router = APIRouter(tags=["WEB PAGE API"])
+admin_router = APIRouter(tags=["WEB PAGE API FOR ADMIN"])
 
-@admin_router.post("/school_group", response_model=ResultResponse, status_code=200)   
+@admin_router.post("/admin/school_group", response_model=ResultResponse, status_code=200)   
 async def school_group(group: SchoolGroupCreate, action: str = Query(...),update_id: Optional[int] = Query(None),db: AsyncSession = Depends(get_db)):
     try:
         if action.lower() == "update_group":
@@ -128,8 +128,7 @@ async def school_group(group: SchoolGroupCreate, action: str = Query(...),update
             detail=str(e)
         )
 
-
-@admin_router.get("/get_school_group_list", response_model=ResultResponse)
+@admin_router.get("/admin/get_school_group_list", response_model=ResultResponse)
 async def get_school_groups(
     school_id: int,
     db: AsyncSession = Depends(get_db),
@@ -158,3 +157,155 @@ async def get_school_groups(
             "data" : data
         }
     )
+
+@admin_router.post("/admin/school_stream", response_model=ResultResponse, status_code=200)   
+async def school_stream(schoolstream: SchoolStreamCreate, action: str = Query(...),update_id: Optional[int] = Query(None) ,db: AsyncSession = Depends(get_db)):
+        try:
+            # ---------- UPDATE STREAM ----------
+            
+            if action.lower() == "update_stream":
+                if not update_id:
+                    return ResultResponse(
+                    code=400,
+                    message="update_id is required for this action"
+                )
+                    
+                stmt = select(SchoolStream).where(SchoolStream.school_group_id == update_id)
+                result = await db.execute(stmt)
+                school_stream = result.scalar_one_or_none()
+                
+                if not school_stream:
+                    return ResultResponse(
+                        code=404,
+                        message="No stream found for this school"
+                    )
+                    
+                school_stream.stream_name = schoolstream.stream_name
+                await db.commit()
+                await db.refresh(school_stream)
+
+                return ResultResponse(
+                    code=200,
+                    status = "Success",
+                    message="Stream name updated successfully",
+                    result = {
+                        "id": school_stream.school_stream_id,
+                        "name": school_stream.stream_name
+                    }
+                )
+            
+            # ---------- CREATE STREAM ----------
+            
+            elif action.lower() == "create_stream":
+                stmt = select(SchoolStream).where(
+                    SchoolStream.school_id == schoolstream.school_id,
+                    SchoolStream.stream_name == schoolstream.stream_code,
+                )
+                
+                result = await db.execute(stmt)
+                existing = result.scalar_one_or_none()
+                if existing:
+                    return ResultResponse(
+                    code=409,
+                    message="School Stream already exist",
+                    status="Failed",
+                    result = {
+                        "group":existing.stream_name,
+                        "status":existing.status})
+                    
+                stream_obj = SchoolStream(**schoolstream.model_dump())
+                db.add(stream_obj)
+                await db.commit()
+                await db.refresh(stream_obj)
+                return ResultResponse(
+                    code=200,
+                    status = "Success",
+                    message="School group created successfully",
+                    result={
+                        "group":stream_obj.stream_name,
+                        "status":stream_obj.status
+                    }
+                )
+            
+            elif action.lower() == "delete_stream":
+                if not update_id:
+                    return ResultResponse(
+                        code=400,
+                        status="Failed",
+                        message="update_id is required for delete_stream"
+                    )
+                    
+                stmt = select(SchoolStream).where(SchoolStream.school_group_id == update_id)
+                result = await db.execute(stmt)
+                school_stream = result.scalar_one_or_none()
+
+                if not school_stream:
+                    return ResultResponse(
+                        code=404,
+                        message="No stream found for this school"
+                    )
+
+                await db.delete(school_stream)
+                await db.commit()
+
+                return ResultResponse(
+                    code=200,
+                    status="Failed",
+                    message="Stream deleted successfully",
+                    result={
+                        "id": school_stream.school_stream_id
+                    }
+                )
+                
+            else:
+                return ResultResponse(
+                    code=400,
+                    status="Failed",
+                    message="Invalid action"
+                )
+                
+        except IntegrityError as e:
+            await db.rollback()
+            raise HTTPException(
+                status_code=400,
+                detail="Database constraint violation {}".format(str(e))
+            )
+        except Exception as e:
+            await db.rollback()
+            raise HTTPException(
+                status_code=500,
+                detail=str(e)
+            )
+
+@admin_router.get("/admin/get_school_stream_list", response_model=ResultResponse)
+async def get_school_stream(
+    db: AsyncSession = Depends(get_db),
+):
+    stmt = select(SchoolStream)
+    result = await db.execute(stmt)
+    groups = result.scalars().all()
+
+    if not groups:
+        return ResultResponse(
+            code=404,
+            status="Failed",
+            message="No stream found for this school"
+        )
+        
+    data = []
+    for stream in groups:
+        data.append({
+            "id": stream.school_stream_id,
+            "stream_name": stream.stream_name,
+            "stream_code":stream.stream_code
+        })
+
+    return ResultResponse(
+        code=200,
+        status = "Success",
+        message="School stream fetched successfully",
+        result = {
+            "data" : data
+        }
+    )
+    
